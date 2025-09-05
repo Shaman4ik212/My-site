9ca9811bb9ea2e222a38bef8b27f0a4c3aef6f42
Shaman4ik212 <strelkovd730@gmail.com>
2025-09-05 18:57:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/task 1")
+@app.route("/")
 def task1():
     return render_template("index.html")
 
-@app.route("/task 2")
-def task2():
-    return "task 2"
 
-if __name__ == ("__main__"):
+
+if __name__ == "__main__":
     serve(app, host='127.0.0.1', port=5000)
